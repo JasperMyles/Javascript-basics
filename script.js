8e26ef3658640a975cd26abc622108d70b5302d8
JasperMyles <ograhjoshua@gmail.com>
2023-11-01 16:04:26 -0700

    I added two folder my html and scriptfile

---
+++ script.js
@@ -0,0 +1,48 @@
+// console.log("hello from our first file")
+
+// let star = 3
+// console.log(star)
+
+// Conditional Statement we start with If - Statement
+
+// if(1 + 1 === 2){
+//     console.log("Maths still works")
+// }
+
+// if(1 + 3 == "4"){
+//     console.log("Cool testing")
+// }
+
+// let userMaths = Math.floor(Math.random() * 4) + 1;
+
+// if(userMaths == 3){
+//     console.log("You got the number right")
+// }
+
+// The start of else if statement
+
+const dayOfWeek = 'Saturday';
+
+if(dayOfWeek === 'Monday'){
+    console.log('I hate M v ondays')
+}
+else if(dayOfWeek === 'Saturday'){
+    console.log('I love Saturdays')
+}
+
+// The start of the else if nothing else is true
+
+let rating = -99;
+
+if(rating === 3){
+    console.log('You are a superstar!');
+}
+else if(rating === 2){
+    console.log('Meets Expectations');
+}
+else if( rating === 1){
+    console.log('Needs Improvement');
+}
+else{
+    console.log('Invalid rating');
+};
