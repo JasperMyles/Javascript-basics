60b3d23ef78093797b8d7c77a972cfbee871203a
JasperMyles <ograhjoshua@gmail.com>
2023-11-03 06:51:50 -0700

    Added a new learning curve for the Logical AND, OR and NOT

--- script.js
+++ script.js
@@ -21,28 +21,104 @@
 
 // The start of else if statement
 
-const dayOfWeek = 'Saturday';
+// const dayOfWeek = 'Saturday';
 
-if(dayOfWeek === 'Monday'){
-    console.log('I hate M v ondays')
-}
-else if(dayOfWeek === 'Saturday'){
-    console.log('I love Saturdays')
-}
+// if(dayOfWeek === 'Monday'){
+//     console.log('I hate M v ondays')
+// }
+// else if(dayOfWeek === 'Saturday'){
+//     console.log('I love Saturdays')
+// }
 
-// The start of the else if nothing else is true
+// // The start of the else if nothing else is true
 
-let rating = -99;
+// let rating = -99;
 
-if(rating === 3){
-    console.log('You are a superstar!');
-}
-else if(rating === 2){
-    console.log('Meets Expectations');
-}
-else if( rating === 1){
-    console.log('Needs Improvement');
-}
-else{
-    console.log('Invalid rating');
-};
+// if(rating === 3){
+//     console.log('You are a superstar!');
+// }
+// else if(rating === 2){
+//     console.log('Meets Expectations');
+// }
+// else if( rating === 1){
+//     console.log('Needs Improvement');
+// }
+// else{
+//     console.log('Invalid rating');
+// };
+
+// const password = prompt("please enter a new password")
+
+// if(password.length >= 6){
+//     if(password.indexOf(' ') === -1){
+//         console.log("Good job! No Space!")
+//     }
+//     else{
+//         console.log("Password cannot contain spaces!")
+//     }
+    
+// }
+// else{
+//     console.log("Password too short")
+// }
+// Nesting conditional statement conbinig logic
+
+
+// Logical operator AND &&, the two statement must be true for the equation to be true
+
+// const passWord = prompt("Enter your password");
+
+// if(password.length >= 6 && password.indexOf('')){
+//     console.log("Incorrect Format for password")
+// } else{
+//     console.log("Incorrect Password")
+// }
+
+// Logical OR operator || if one side is true, the entire thing is true"
+
+// const age = 100;
+// if(age >= 0 && age < 5 || age >= 65){
+//     console.log("FREE")
+// } else if( age >= 5 && age < 10){
+//     console.log("$10")
+// } else if(age >= 10 && age < 65){
+//     console.log("$20")
+// } else{
+//     console.log('INVALID AGE!')
+// }
+
+//  Logical NOT ! expression returns true if expression is false - it flips the booleaness of the value
+
+// const firstName = prompt("Enter Your FirstName");
+// if(!firstName){
+//     firstName = prompt("Try Again!!");
+// }
+
+//  Switch Statement is another control flow statement that can replace multiple if statements
+
+// const day = 2;
+//  switch (day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5: 
+//         console.log("Friday");
+//         break;
+//     default:
+//         console.log("I do not know that")
+
+//  }
+
+// JAVASCRIPT ARRAYS START OF THE SECTION
+
+
+
